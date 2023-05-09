b15dabeca33125dc557f38d05213909be8cd7e9a
Oliver Fu <ofu997@gmail.com>
2023-05-08 17:18:29 -0700

    add dictionary for encryption

--- Controllers/ResponsesController.cs
+++ Controllers/ResponsesController.cs
@@ -65,7 +65,17 @@ namespace Exp.Controllers
         {
             if (ModelState.IsValid)
             {
-                string apiKey = "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";
+                string apiKey = Decipher( new int[] 
+                    { 8, 16, 53, 54, 59, 10, // sk-16q
+                    52, 51, 60, 16, 36, 24, // AB7kQc
+                    43, 59, 35, 55, 36, 17, // J6R2Qj
+                    27, 39, 31, 62, 13, 33, // ZNV9nT
+                    56, 51, 15, 25, 16, 47, // 3BlbkF
+                    43, 62, 48, 11, 49, 18, // J9EqDi J9EpDi
+                    62, 19, 52, 27, 3, 28, // 9hAZxY
+                   8, 38, 57, 2, 9, 49, 37, // sO4yrDP 
+                    57, 33 } // 4T
+                );
                 string answer = string.Empty;
                 var openai = new OpenAIAPI(apiKey);
                 CompletionRequest completion = new CompletionRequest();
@@ -84,13 +94,65 @@ namespace Exp.Controllers
                 response.Result = answer;
 
                 response.Id = Guid.NewGuid();
+                _context.Add(response);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
 
-            _context.Add(response);
+            //_context.Response.Add(response); 
 
             return View(response);
+        }
+
+        public string Decipher(int[] encryption)
+        {
+            Dictionary<int, char> Encryptions = new Dictionary<int, char>()
+            {
+                { 1,'z' }, { 27, 'Z'},
+                { 2, 'y' }, { 28, 'Y'},
+                { 3, 'x' }, { 29, 'X'},
+                { 4, 'w' }, { 30, 'W'},
+                { 5, 'v' }, { 31, 'V'},
+                { 6, 'u' }, { 32, 'U'},
+                { 7, 't' }, { 33, 'T'},
+                { 8, 's' }, { 34, 'S'},
+                { 9, 'r' }, { 35, 'R'},
+                { 10, 'q' }, { 36, 'Q'},
+                { 11, 'p' }, { 37, 'P'},
+                { 12, 'o' }, { 38, 'O'},
+                { 13, 'n' }, { 39, 'N'},
+                { 14, 'm' }, { 40, 'M'},
+                { 15, 'l' }, { 41, 'L'},
+                { 16, 'k' }, { 42, 'K'},
+                { 17, 'j' }, { 43, 'J'},
+                { 18, 'i' }, { 44, 'I'},
+                { 19, 'h' }, { 45, 'H'},
+                { 20, 'g' }, { 46, 'G'},
+                { 21, 'f' }, { 47, 'F'},
+                { 22, 'e' }, { 48, 'E'},
+                { 23, 'd' }, { 49, 'D'},
+                { 24, 'c' }, { 50, 'C'},
+                { 25, 'b' }, { 51, 'B'},
+                { 26, 'a' }, { 52, 'A'},
+
+                { 53,  '-'},
+                { 54, '1' },
+                { 55, '2' },
+                { 56, '3' },
+                { 57, '4' },
+                { 58, '5' },
+                { 59, '6' },
+                { 60, '7' },
+                { 61, '8' },
+                { 62, '9' },
+                { 63, '0' }
+            };
+            var resultString = "";
+            foreach (var ch in encryption)
+            {
+                resultString += Encryptions[ch];
+            }
+            return resultString; 
         }
 
         // GET: Responses/Edit/5
